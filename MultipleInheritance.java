f902d02685fe533e603cb53485291daee800e8bc
Kartik Badarinarayana <30652456+kartik20393@users.noreply.github.com>
2026-04-26 14:16:34 +0530

    Implement multiple inheritance using interfaces

---
+++ MultipleInheritance.java
@@ -0,0 +1,25 @@
+interface Animal {
+    void eat();
+}
+
+interface Bird {
+    void fly();
+}
+
+class Bat implements Animal, Bird {
+    public void eat() {
+        System.out.println("Bat eats insects");
+    }
+
+    public void fly() {
+        System.out.println("Bat can fly");
+    }
+}
+
+public class MultipleInheritance {
+    public static void main(String[] args) {
+        Bat bat = new Bat();
+        bat.eat();
+        bat.fly();
+    }
+}
