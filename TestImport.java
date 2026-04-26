4a004e6b21d6be9908b4217974340522620a9c25
Kartik Badarinarayana <30652456+kartik20393@users.noreply.github.com>
2026-04-26 14:27:45 +0530

    Add TestImport class to demonstrate MyClass usage

---
+++ TestImport.java
@@ -0,0 +1,8 @@
+import mypackage.MyClass; // Import statement
+
+public class TestImport {
+    public static void main(String[] args) {
+        MyClass obj = new MyClass();
+        obj.display();
+    }
+}
