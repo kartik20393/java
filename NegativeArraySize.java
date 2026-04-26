052c20cd385224424ca65848a834f235452adc89
Kartik Badarinarayana <30652456+kartik20393@users.noreply.github.com>
2026-04-26 14:21:24 +0530

    Add NegativeArraySize class to handle exceptions

---
+++ NegativeArraySize.java
@@ -0,0 +1,9 @@
+public class NegativeArraySize {
+    public static void main(String[] args) {
+        try {
+            int[] arr = new int[-5];
+        } catch (NegativeArraySizeException e) {
+            System.out.println("NegativeArraySizeException caught");
+        }
+    }
+}
